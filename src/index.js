f6cb7748f60bf2d4dc0713fccf2c384ebf645045
mloherr <herraizmaria08@gmail.com>
2024-06-20 12:37:16 +0200

    created succesfully petition to API with book list

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+async function getDBConnection() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectTimeout: 30000,
+  });
+  connection.connect();
+  return connection;
+}
+
+const port = process.env.PORT || 4002;
+server.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
+
+server.get('/book-list', async (req, res) => {
+  const connection = await getDBConnection();
+  const querySQL = 'SELECT * FROM bookslist';
+  const [result] = await connection.query(querySQL);
+  res.json({
+    info: { count: result.length },
+    results: result,
+  });
+  connection.end();
+});
+
+const staticServer = './src/public-react';
+server.use(express.static(staticServer));
--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -2,9 +2,23 @@ import '../scss/App.scss';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import api from '../services/api';
+import { useState, useEffect } from 'react';
 // import PropTypes from "prop-types";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api.getBooks().then((response) => {
+      console.log(response);
+      setBooks(response.results);
+      console.log(response.results);
+      setIsLoading(false);
+    });
+  }, []);
   return (
     <>
       <Header />
